8307f51f0b5858eeff363fd0728e9f233de9c89a
shawnbutts <me@shawnbutts.com>
2016-09-23 14:33:53 -0400

    fix for cassandra metrics

--- src/collectors/jolokia/cassandra_jolokia.py
+++ src/collectors/jolokia/cassandra_jolokia.py
@@ -30,6 +30,7 @@ import math
 import string
 import re
 
+
 class CassandraJolokiaCollector(JolokiaCollector):
     # override to allow setting which percentiles will be collected
 
@@ -113,13 +114,13 @@ class CassandraJolokiaCollector(JolokiaCollector):
             last_num = next_num
 
         return offsets
-    
+
     ########
     #
     # The clean_up() function takes the metric name and cleans it up to make it
     # more presentable.  The default Jolokia JMX collector only does some minimal
     # work in this regard (replacing '=' and ':" with '_" and things like that),
-    # whereas for Cassandra we need to actually rearrange the ordering of the 
+    # whereas for Cassandra we need to actually rearrange the ordering of the
     # JMX keys to make the metrics fit into a hierarchy.  Therefore, we override
     # the clean_up method to do just that.
     #
@@ -129,15 +130,17 @@ class CassandraJolokiaCollector(JolokiaCollector):
 
     def clean_up(self, text):
 
-        # The metric name as it comes in has two main parts separated by a colon.
+        # The metric name as it comes in has two main parts separated by a
+        # colon.
         s = string.split(text, ':')
 
-        # We don't care about the first part, but the second part contains all of the JMX keys.
+        # We don't care about the first part, but the second part contains all
+        # of the JMX keys.
         jmx_keys = s[1]
 
         # Some metrics also have a statistic (i.e., "Min", "Max", "OneMinuteRate", "Value")
         # Not all metrics have this, but those that do will always have it at the end of
-        # the list of JMX keys, separated by a dot.  Since the keys are separated by commas, 
+        # the list of JMX keys, separated by a dot.  Since the keys are separated by commas,
         # the first thing we do is look for the last comma in the string.
         statistic = ''
         last_comma_index = string.rfind(jmx_keys, ',')
@@ -147,18 +150,23 @@ class CassandraJolokiaCollector(JolokiaCollector):
                 # Now find the last dot that occurs in this final key's value.
                 # (NOTE: We can't just search the entire JMX keys string for the last dot,
                 # because if the metric in question does not have a statstic at the end,
-                # and one of the other keys has dots in it, then we won't parse things correctly.)
-                last_dot_index = string.rfind(jmx_keys, '.', last_comma_index)
-                # If there wasn't a dot, this just means that this metric didn't have a statistic on the end
-                if (-1 != last_dot_index):
-                        # If there was a dot, parse out the statistic, and strip it off the jmx_keys
-                        statistic = jmx_keys[last_dot_index+1:]
-                        jmx_keys = jmx_keys[:last_dot_index]
+                # and one of the other keys has dots in it, then we won't parse
+                # things correctly.)
+            last_dot_index = string.rfind(jmx_keys, '.', last_comma_index)
+            # If there wasn't a dot, this just means that this metric didn't
+            # have a statistic on the end
+            if (-1 != last_dot_index):
+                # If there was a dot, parse out the statistic, and strip it off
+                # the jmx_keys
+                statistic = jmx_keys[last_dot_index + 1:]
+                jmx_keys = jmx_keys[:last_dot_index]
 
-        # Parse the jmx_keys string to split everything out into an array of name-value pairs.
+        # Parse the jmx_keys string to split everything out into an array of
+        # name-value pairs.
         kvps = string.split(jmx_keys, ',')
 
-        # Initialize the variables that will be used to hold the values from the JXM keys
+        # Initialize the variables that will be used to hold the values from
+        # the JXM keys
         m_type = ''
         keyspace = ''
         name = ''
@@ -168,61 +176,69 @@ class CassandraJolokiaCollector(JolokiaCollector):
 
         # Loop through the array of name-value pairs.
         for i in range(len(kvps)):
-                # Split the pair on the equal sign such that kvp[0] is the key and kvp[1] is the value
-                kvp = string.split(kvps[i], '=')
-                # Replace any dots in the values with dashes
-                kvp[1] = string.replace(kvp[1], '.', '-')
-                # Check the name of the key, and set the appropriate variable accordingly
-                if (kvp[0].lower() == 'type'):
-                        m_type = kvp[1]
-                elif (kvp[0].lower() == 'keyspace'):
-                        keyspace = '.' + kvp[1]
-                elif (kvp[0].lower() == 'name'):
-                        name = '.' + kvp[1]
-                elif (kvp[0].lower() == 'scope'):
-                        scope = '.' + kvp[1]
-                elif (kvp[0].lower() == 'columnfamily'):
-                        colfam = '.' + kvp[1]
-                elif (kvp[0].lower() == 'path'):
-                        path = '.' + kvp[1]
-                else:
-                        self.log.error('Unknown key name: %s', kvps[i])
+            # Split the pair on the equal sign such that kvp[0] is the key and
+            # kvp[1] is the value
+            kvp = string.split(kvps[i], '=')
+            # Replace any dots in the values with dashes
+            kvp[1] = string.replace(kvp[1], '.', '-')
+            # Check the name of the key, and set the appropriate variable
+            # accordingly
+            if (kvp[0].lower() == 'type'):
+                m_type = kvp[1]
+            elif (kvp[0].lower() == 'keyspace'):
+                keyspace = '.' + kvp[1]
+            elif (kvp[0].lower() == 'name'):
+                name = '.' + kvp[1]
+            elif (kvp[0].lower() == 'scope'):
+                scope = '.' + kvp[1]
+            elif (kvp[0].lower() == 'columnfamily'):
+                colfam = '.' + kvp[1]
+            elif (kvp[0].lower() == 'path'):
+                path = '.' + kvp[1]
+            else:
+                self.log.error('Unknown key name: %s', kvps[i])
 
-        # Now we need to build the metric name, which happens in slightly different ways depending on the metric type.
+        # Now we need to build the metric name, which happens in slightly
+        # different ways depending on the metric type.
 
         # If the metric type is a column family metric, then the metric is either:
         #   1) Associated with a particluar table in a particular keyspace; or
-        #   2) An aggregate across all tables and keyspaces in the Cassandra node.
+        # 2) An aggregate across all tables and keyspaces in the Cassandra
+        # node.
         if (m_type == 'ColumnFamily') or (m_type == 'ColumnFamilies'):
-                if (keyspace != ''):
-                        # If the keyspace is not blank, it's a metric for a specific keyspace and table
-                        metric_name = 'Keyspace._Keyspaces' + keyspace + '._Tables' + scope + name
-                else:
-                        # Otherwise, it's a global aggregate
-                        metric_name = 'Keyspace' + scope + name
+            if (keyspace != ''):
+                # If the keyspace is not blank, it's a metric for a specific
+                # keyspace and table
+                metric_name = 'Keyspace._Keyspaces' + keyspace + '._Tables' + scope + name
+            else:
+                # Otherwise, it's a global aggregate
+                metric_name = 'Keyspace' + scope + name
         # If the metric type is a keyspace metric, then the metric is either:
         #   1) An aggregate across all tables in a particular keyspace; or
-        #   2) An aggregate across all tables and keyspaces in the Cassandra node.
+        # 2) An aggregate across all tables and keyspaces in the Cassandra
+        # node.
         elif (m_type == 'Keyspace'):
-                if (keyspace != ''):
-                        # If the keyspace is not blank, it's an aggrgate metric for a specific keyspace
-                        metric_name = m_type + '._Keyspaces' + keyspace + scope + name
-                else:
-                        # Otherwise, it's a global aggregate
-                        metric_name = 'Keyspace' + scope + name
-        # If the metric is a thread pool metric, there is an additional 'path' component to add to the metric name
+            if (keyspace != ''):
+                # If the keyspace is not blank, it's an aggrgate metric for a
+                # specific keyspace
+                metric_name = m_type + '._Keyspaces' + keyspace + scope + name
+            else:
+                # Otherwise, it's a global aggregate
+                metric_name = 'Keyspace' + scope + name
+        # If the metric is a thread pool metric, there is an additional 'path'
+        # component to add to the metric name
         elif (m_type == 'ThreadPool'):
-                 metric_name = m_type + path + scope + name
+            metric_name = m_type + path + scope + name
         # All other metrics are constructed simply as type + scope + name
         else:
-                metric_name = m_type + scope + name
+            metric_name = m_type + scope + name
 
         # If there is a statistic, we append that to the metric name.
         # (NOTE: We don't append the statistic if it's 'Value', since metrics with 'Value' don't have
         # any other statistics, so this only results in the user having an extra layer to click through
         # in the metric name hierarchy.)
         if (statistic != '') and (statistic != 'Value'):
-                metric_name = metric_name + '.' + statistic
+            metric_name = metric_name + '.' + statistic
 
         # Finally, return the new metric name.
-        return metric_name                        
+        return metric_name
