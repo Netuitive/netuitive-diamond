d1164c953b2526e75c3b232439d6d5043427d9b7
codytipton <cody.tipton@metricly.com>
2018-11-28 10:02:49 -0600

    Instanciate concurrent log handler

---
+++ src/diamond/logging/handlers/concurrent-log-handler.py
@@ -0,0 +1,18 @@
+# coding=utf-8
+
+from concurrent_log_handler import ConcurrentRotatingFileHandler as CRFH
+import sys
+
+
+class ConcurrentRotatingFileHandler(TRFH):
+
+    def flush(self):
+        try:
+            super(ConcurrentRotatingFileHandler, self).flush()
+        except IOError:
+            sys.stderr.write('ConcurrentRotatingFileHandler received a IOError!')
+            sys.stderr.flush()
+
+
+
+
